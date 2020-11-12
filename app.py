1ccdca5ce3ac2a9fb0579c1b252c34b4c6661ca8
jack <jaitk11@gmail.com>
2020-11-12 08:31:55 -0500

    still in testing, but making changes

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import config
+import helpers
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
+    else:
+        title = request.form.get('title')
+        author = request.form.get('author')
+
+
+    
